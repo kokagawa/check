2a4482348aa6fb23c3a56a9b7f7368ee922f75fe
kagawa <tekken77marduk>
2020-02-04 20:23:36 +0900

    Update alarm.js

--- app/assets/javascripts/alarm.js
+++ app/assets/javascripts/alarm.js
@@ -66,6 +66,7 @@ var al_date = new Date();
         if (res < 1260) {
         timer1  = setInterval(move, (gon.checktime.total_sec / 1250) * 1000);
         console.log(gon.checktime.total_sec);
-        console.log((gon.checktime.total_sec/ 1250) * 1000);
+        console.log((gon.checktime.total_sec / 1250) * 1000);
+        console.log(10000 / 1200)
         }
 })    
