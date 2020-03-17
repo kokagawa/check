2231eab7cf6395af159f18bde551f897f55c2601
kagawa <tekken77marduk>
2020-03-17 17:16:11 +0900

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'checktimes#new' 
   resources :alarms
   resources :checktimes
   resources :description
+  root to: "home#index"
   
 
 
