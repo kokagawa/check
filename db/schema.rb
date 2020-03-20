4154f639c844a7978b4dd426ab76b66a4cdb0bff
kagawa <tekken77marduk>
2020-03-20 19:58:55 +0900

    user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_075751) do
+ActiveRecord::Schema.define(version: 2020_03_20_105613) do
 
   create_table "checktimes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "plan", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_075751) do
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
